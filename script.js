9c43a5f30df7d04c8aa199c6c9203c5e611b2c62
Nikita <110771527+Racio-begin@users.noreply.github.com>
2022-09-04 18:37:58 +0300

    Init5
    Доработаны иконки социальных сетей

---
+++ script.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('.button');
+
+// button.addEventListener('click', function () {
+//   let randomElement = getRandomElement(phrases);
+//   smoothly(phrase, 'textContent', randomElement.text);
+//   smoothly(image, 'src', randomElement.image);
